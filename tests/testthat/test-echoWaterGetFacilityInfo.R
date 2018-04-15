43de8d728107fec3f0bcc67e50d0ca604e370a0a
Michael Schramm <mpschramm@gmail.com>
2018-04-15 09:50:37 -0500

    Add new unit tests
    Add tests to complete coverage of echoWaterGetFacilityInfo and
    echoAirGetFacilityInfo

--- tests/testthat/test-echoWaterGetFacilityInfo.R
+++ tests/testthat/test-echoWaterGetFacilityInfo.R
@@ -12,8 +12,32 @@ library(here)
 
 
 with_mock_api({
-  test_that("echoWaterGetFacilityInfo works", {
+  test_that("echoWaterGetFacilityInfo df works", {
     x <- echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "df")
     expect_equal(is.data.frame(x), TRUE)
+
+    expect_error(echoWaterGetFacilityInfo(), "No valid arguments supplied")
+
+    expect_message(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "df", verbose = TRUE))
+
+    expect_error(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "JSON"))
     })
 })
+
+
+# capture_requests({
+#   echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf")
+#   })
+
+with_mock_api({
+  test_that("echoWaterGetFacilityInfo sf works", {
+    x <- echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf")
+
+    expect_equal(is(x, "sf"), TRUE)
+
+    expect_message(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf", verbose = TRUE))
+  })
+})
+
+
+
