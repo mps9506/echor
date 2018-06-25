6068d0326a0040a6f300cd608a2af2175933594d
mps9506 <mpschramm@gmail.com>
2018-06-25 13:14:58 -0500

    :white_check_mark: Update unit tests
    Remove httptest with_mock_api usage. Replace with skip_on_cran.

--- tests/testthat/test-echoAirGetFacilityInfo.R
+++ tests/testthat/test-echoAirGetFacilityInfo.R
@@ -1,43 +1,22 @@
 context("test-echoAirGetFacilityInfo")
 
-library(httptest)
-library(here)
 
-.mockPaths(here("tests/testthat/"))
+test_that("echoAirGetFacilityInfo returns a df", {
+  skip_on_cran()
+  x <- echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "df")
+  expect_equal(is.data.frame(x), TRUE)
 
-
-# test_that("Record requests if online", {
-#   capture_requests({
-#   echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "df")
-#   })
-#
-# })
-
-with_mock_api({
-  test_that("echoAirGetFacilityInfo works", {
-    x <- echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "df")
-    expect_equal(is.data.frame(x), TRUE)
-
-    expect_error(echoAirGetFacilityInfo(), "No valid arguments supplied")
-
-    expect_message(echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "df", verbose = TRUE))
-
-    expect_error(echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "JSON", verbose = TRUE))
+  x <- echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "sf")
+  expect_equal(is(x, "sf"), TRUE)
   })
-})
 
-
-
-# capture_requests({
-#   echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "sf")
-#   })
-
-with_mock_api({
-  test_that("echoWaterGetFacilityInfo sf works", {
-    x <- echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "sf")
-
-    expect_equal(is(x, "sf"), TRUE)
-
-    expect_message(echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "sf", verbose = TRUE))
+test_that("echoAirGetFacilityInfo returns errors", {
+  expect_error(echoAirGetFacilityInfo(), "No valid arguments supplied")
+  expect_error(echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "JSON", verbose = TRUE))
   })
-})
+
+test_that("echoAirGetFacilityInfo produces messages", {
+  skip_on_cran()
+  expect_message(echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "df", verbose = TRUE))
+  expect_message(echoAirGetFacilityInfo(p_pid = "NC0000003706500036", output = "sf", verbose = TRUE))
+  })
--- tests/testthat/test-echoGetEffluent.R
+++ tests/testthat/test-echoGetEffluent.R
@@ -1,18 +1,8 @@
 context("test-getEffluent")
 
-library(httptest)
-library(here)
 
-.mockPaths(here("tests/testthat/"))
-
-# capture_requests({
-#    echoGetEffluent(p_id = "tx0119407", parameter_code = "50050")
-#  })
-
-
-with_mock_api({
-  test_that("echoGetEffluent works", {
-    x <- echoGetEffluent(p_id = "tx0119407", parameter_code = "50050")
-    expect_equal(is.data.frame(x), TRUE)
+test_that("echoGetEffluent works", {
+  skip_on_cran()
+  x <- echoGetEffluent(p_id = "tx0119407", parameter_code = "50050")
+  expect_equal(is.data.frame(x), TRUE)
   })
-})
