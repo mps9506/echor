477d6e6d81137e4d900d208f650dca4682a130b7
mps9506 <mpschramm@gmail.com>
2018-06-25 12:42:35 -0500

    :white_check_mark: Unit test update
    Remove reliance on httptest to do unittests. Locally stored JSONs are too heavy. skip_on_cran instead.

--- tests/testthat/test-echoWaterGetFacilityInfo.R
+++ tests/testthat/test-echoWaterGetFacilityInfo.R
@@ -1,40 +1,24 @@
 context("test-echoWatergetfacilityInfo")
 
-library(httptest)
-library(here)
+test_that("echoWaterGetFacilityInfo returns df", {
+  skip_on_cran()
+  x <- echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "df")
+  expect_equal(is.data.frame(x), TRUE)
+  })
 
-.mockPaths(here("tests/testthat/"))
-
-
-# capture_requests({
-#   echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "df")
-#   })
-
-
-with_mock_api({
-  test_that("echoWaterGetFacilityInfo df works", {
-    x <- echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "df")
-    expect_equal(is.data.frame(x), TRUE)
-
-    expect_error(echoWaterGetFacilityInfo(), "No valid arguments supplied")
-
-    expect_message(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "df", verbose = TRUE))
-
-    expect_error(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "JSON"))
-    })
+test_that("echoWaterGetFacilityInfo returns errors",{
+  expect_error(echoWaterGetFacilityInfo(), "No valid arguments supplied")
+  expect_error(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "JSON"))
 })
 
+test_that("echoWaterGetFacilityInfo returns messages", {
+  skip_on_cran()
+  expect_message(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "df", verbose = TRUE))
+  expect_message(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf", verbose = TRUE))
+})
 
-# capture_requests({
-#   echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf")
-#   })
-
-with_mock_api({
-  test_that("echoWaterGetFacilityInfo sf works", {
-    x <- echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf")
-
-    expect_equal(is(x, "sf"), TRUE)
-
-    expect_message(echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf", verbose = TRUE))
-  })
+test_that("echoWaterGetFacilityInfo returns sf", {
+  skip_on_cran()
+  x <- echoWaterGetFacilityInfo(p_pid = "ALR040033", output = "sf")
+  expect_equal(is(x, "sf"), TRUE)
 })
