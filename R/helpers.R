7189478ffc3718ca35dd53184717064aee0ed241
Michael Schramm <mpschramm@gmail.com>
2019-01-16 16:39:59 -0600

    add downloadDMRs()
    * addresses #38
    * new function loops through list of permit id's and downloads dmr permits.

---
+++ R/helpers.R
@@ -0,0 +1,50 @@
+
+# downloadDMRs ------------------------------------------------------------
+
+
+#' Title
+#'
+#' @param df dataframe with column of id numbers
+#' @param nColumn numeric, indicates the column with p_id permit numbers
+#' @param pBar logical, display a progress bar? Defaults to TRUE
+#' @param ... additional arguments passed to echoGetEffluentSummary
+#'
+#' @import dplyr
+#' @import rlang
+#' @return dataframe df, with a column containing the discharge monitoring reports downloaded with echoGetEffluentSummary
+#' @export
+
+downloadDMRs <- function(df, nColumn, pBar = TRUE, ...) {
+
+  nColumn <- enquo(nColumn)
+  data <- select(df, !!nColumn)
+
+  if (isTRUE(pBar)) {
+    # create the progress bar with a dplyr function.
+    pb <- progress_estimated(nrow(df))
+
+    df <- df %>%
+      mutate(dmr = purrr::pmap(data,
+                               ~ {
+                                 # update the progress bar (tick()) and print progress (print())
+                                 pb$tick()$print()
+
+
+                                 echoGetEffluentSummary(p_id = ..1,
+                                                        verbose = FALSE,
+                                                        ...)
+                               }))
+  }
+
+  else {
+    df <- df %>%
+      mutate(dmr = purrr::pmap(data,
+                               ~ {
+                                 echoGetEffluentSummary(p_id = ..1,
+                                                        verbose = FALSE,
+                                                        ...)
+                               }))
+  }
+
+  return(df)
+}
