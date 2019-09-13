633d86a53486b4052e8113ef9b82b48028f1d27d
mps9506 <mpschramm@gmail.com>
2019-09-13 13:09:17 -0500

    downloadEffluentChart returns all columns as characters by default

--- R/water.R
+++ R/water.R
@@ -224,21 +224,12 @@ downloadEffluentChart <- function(p_id, verbose, queryDots) {
   info <- httr::content(request, as = "raw")
 
   info <- readr::read_csv(info, col_names = TRUE,
-                          ## Would prefer col_types not be hard-coded in case csv changes format later
-                          ## this just specs the columns that readr reads incorrectly
-                          ## col_types = "cccccccccccDDccccccccccdccccdcccccccDccccdccdcDcccdcccccccccc",
                           col_types = readr::cols(
-                            activity_id = readr::col_character(),
-                            perm_feature_id = readr::col_character(),
-                            limit_value_id = readr::col_character(),
-                            dmr_event_id = readr::col_character(),
-                            dmr_form_value_id = readr::col_character(),
-                            dmr_value_id = readr::col_character(),
-                            npdes_violation_id = readr::col_character()
+                            .default = readr::col_character()
                           ),
                           na = " ",
                           locale = readr::locale(date_format = "%m/%d/%Y"),
-                          progress = FALSE)
+                          progress = FALSE, skip_empty_rows = FALSE)
   return(info)
 }
 
