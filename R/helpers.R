091e6b810f981760b6e5b72bd735941f9841ed08
Michael Schramm <mpschramm@gmail.com>
2019-09-17 19:21:28 -0500

    fix example

--- R/helpers.R
+++ R/helpers.R
@@ -21,7 +21,7 @@
 #'
 #' ## Retrieve multiple DMRs for flow
 #'
-#' df <- data_frame("p_id" = c('TX0119407', 'TX0132187', 'TX040237'))
+#' df <- tibble("p_id" = c('TX0119407', 'TX0132187', 'TX040237'))
 #' df <- downloadDMRs(df, p_id)
 #' }
 
